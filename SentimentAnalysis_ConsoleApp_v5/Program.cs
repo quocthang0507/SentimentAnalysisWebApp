a01497d1d33ec4842f555c4e81a3999991e54017
quocthang0507 <quocthang_0507@yahoo.com.vn>
2021-07-11 08:14:46 +0700

    Update project v5

--- SentimentAnalysis_ConsoleApp_v5/Program.cs
+++ SentimentAnalysis_ConsoleApp_v5/Program.cs
@@ -6,7 +6,24 @@ namespace SentimentAnalysis_ConsoleApp_v5
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			// Create single instance of sample data from first line of dataset for model input
+			SentimentAnalysisModel.ModelInput sampleData = new SentimentAnalysisModel.ModelInput()
+			{
+				Col3 = @"Đồ giống hình, nhìn khá xinh, giá rẻ, ship siêu nhanh luôn. Chưa dùng thử nên chưa biết thế nào",
+			};
+
+			// Make a single prediction on the sample data and print results
+			var predictionResult = SentimentAnalysisModel.Predict(sampleData);
+
+			Console.WriteLine("Using model to make single prediction -- Comparing actual Col2 with predicted Col2 from sample data...\n\n");
+
+
+			Console.WriteLine($"Col3: {sampleData.Comment}");
+
+
+			Console.WriteLine($"\n\nPredicted Col2: {predictionResult.Prediction}\n\n");
+			Console.WriteLine("=============== End of process, hit any key to finish ===============");
+			Console.ReadKey();
 		}
 	}
 }
